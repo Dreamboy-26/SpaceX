ccf8f3aeb5de8959171ce69a5490ffe69493b32f
Dreamboy-26 <heyimpankaj2616@gmail.com>
2022-12-15 15:00:45 +0530

    folder structure done

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='NavContainer'>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div></div>
+
+    </div>
+  )
+}
+
+export default Navbar
